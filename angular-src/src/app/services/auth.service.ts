e07d57b5f50843a1d2accbf4afe5cec1b14886a9
Josh Armstrong <josharmstrongpdx@gmail.com>
2019-02-19 12:44:20 -0800

    Created protected pages
    Created pages that can only be used when authorized (logged in). If someone tried to view a page through the URL bar it would redirect them to login since they're not authorized. Changed navbar to only show page links when logged in.

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -14,15 +15,24 @@ export class AuthService {
   registerUser(user){
     let headers = new HttpHeaders();
     headers.append('Content-Type', 'application/json');
-    return this.http.post('http://localhost:3000/users/register', user, {headers: headers})
+    return this.http.post('users/register', user, {headers: headers})
       .pipe(map((response: any) => response)); 
   }
 
   authenticateUser(user){
     let headers = new HttpHeaders();
     headers.append('Content-Type', 'application/json');
-    return this.http.post('http://localhost:3000/users/authenticate', user, {headers: headers})
+    return this.http.post('users/authenticate', user, {headers: headers})
       .pipe(map((response: any) => response)); 
+  }
+
+  getProfile(){
+    let headers = new HttpHeaders();
+    this.loadToken();
+    headers = headers
+      .set('Content-Type', 'application/json')
+      .set('Authorization', this.authToken);
+    return this.http.get<any>('users/profile', { headers: headers });
   }
 
   storeUserData(token, user){
@@ -32,9 +42,21 @@ export class AuthService {
     this.user = user;
   }
 
-  logout(token, user){
-    localStorage.setItem('id_token', token);
-    localStorage.setItem('user', JSON.stringify(user));
+  loadToken(){
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
+
+  loggedIn(){
+    if (localStorage.id_token == undefined ){
+     return false;
+    } else {
+    const helper = new JwtHelperService();
+    return !helper.isTokenExpired(localStorage.id_token);
+    }
+  }
+
+  logout() {
     this.authToken = null;
     this.user = null;
     localStorage.clear();
